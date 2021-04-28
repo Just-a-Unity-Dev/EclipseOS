7e185e838b9153314b78de626883d79dcc07570e
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 20:11:20 +0400

    just put the normal wiki.js file back, and not test

--- Terminal/wikipedia.js
+++ Terminal/wikipedia.js
@@ -1,25 +1,24 @@
 async function performSearch(searchQuery) {
-    const endpoint = 'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch='+searchQuery;
-    const response = await fetch(endpoint);
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    const json = await response.json();
-    return json
+  const endpoint = 'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch='+searchQuery;
+  const response = await fetch(endpoint);
+  if (!response.ok) {
+    throw Error(response.statusText);
   }
-  async function wikipediaSearch(results) {
+  const json = await response.json();
+  return json
+}
+async function wikipediaSearch(results) {
 
-    results.query.search.forEach(result => {
-      const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
-  
-      const text =
-        `
-            <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
-          </h3>
-          <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
-          <p>${result.snippet}</p><br>
-        `;
-        return text;
-    });
-  }
-  
+  results.query.search.forEach(result => {
+    const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+
+    const text =
+      `
+          <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+        </h3>
+        <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
+        <p>${result.snippet}</p><br>
+      `;
+      return text;
+  });
+}
